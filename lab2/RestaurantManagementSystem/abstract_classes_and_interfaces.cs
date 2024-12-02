4ed04cf4ed370f0902f8c9ba9f4c0245028d7a2c
Zahar-Bratchikov <zaharbratcikov09817@gmail.com>
2024-12-03 01:57:35 +0500

    Начал выполнять лабу 2

---
+++ lab2/RestaurantManagementSystem/abstract_classes_and_interfaces.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RestaurantManagementSystem
+{
+    public abstract class MenuItem
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public float Price { get; set; }
+
+        public abstract float GetCost();
+    }
+}
