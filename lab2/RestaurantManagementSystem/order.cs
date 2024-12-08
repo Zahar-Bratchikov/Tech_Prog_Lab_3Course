10ca877485491a6df297fa339330dbe4ef993670
Zahar-Bratchikov <zaharbratcikov09817@gmail.com>
2024-12-09 00:43:31 +0500

    Реализовал классы Order и Client, так же добавил .gitignore

--- lab2/RestaurantManagementSystem/order.cs
+++ lab2/RestaurantManagementSystem/order.cs
@@ -1,17 +1,80 @@
 ﻿namespace RestaurantManagementSystem
 {
-    public interface OrderObserver
+    public interface IClientObserver
     {
+        void UpdateStatus(OrderStatus status);
+    }
 
+    public enum OrderStatus
+    {
+        Created, Preparing, Completed, Cancelled
     }
 
    public class Order
     {
+        private List<MenuItem> orderItems = new();
+        private OrderStatus status;
+        private List<IClientObserver> observers = new();
 
+        private void notifyClients()
+        {
+            foreach (var observer in observers)
+            {
+                observer.UpdateStatus(status);
+            }
+        }
+
+        public Order()
+        {
+            status = OrderStatus.Created;
+        }
+
+        public void addMemuItem(MenuItem item) 
+        {
+            orderItems.Add(item);
+        }
+
+        public void removeMemuItem(MenuItem item)
+        {
+            orderItems.Remove(item);
+        }
+
+        public void changeStatus(OrderStatus newStatus)
+        {
+            status = newStatus;
+            notifyClients();
+        }
+
+        public void attach(IClientObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void displayOrder()
+        {
+            Console.WriteLine("Ordered items:");
+            foreach (var item in orderItems)
+            {
+                Console.WriteLine($"{item.Name}: {item.GetCost()} c.u.");
+            }
+        }
+
+        public OrderStatus Status => status;
     }
 
-    public class Client
+    public class Client : IClientObserver
     {
+        public string name { get; set; }
+        public string contactInfo { get; set; }
 
+        public void createOrder()
+        {
+            Console.WriteLine($"{name} create order");
+        }
+
+        public void UpdateStatus(OrderStatus status)
+        {
+            Console.WriteLine($"Уведомление для клиента {name}: Статус заказа изменен на {status}.");
+        }
     }
 }
