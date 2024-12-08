8033311b607eb5a73b757518e3edc89beea712f2
Zahar-Bratchikov <zaharbratcikov09817@gmail.com>
2024-12-09 02:31:01 +0500

    Реализовал  Employee

--- lab2/RestaurantManagementSystem/employee.cs
+++ lab2/RestaurantManagementSystem/employee.cs
@@ -1,22 +1,59 @@
 ﻿namespace RestaurantManagementSystem
 {
-    public class Employee
+    public abstract class Employee
     {
+        public Employee? nextEmployee;
 
+        public void SetNextEmployee(Employee next)
+        {
+            nextEmployee = next;
+        }
+
+        public abstract void PerformTask(string task);
     }
 
-    public class Waiter
+    public class Waiter : Employee
     {
-
+        public override void PerformTask(string task)
+        {
+            if (task == "Serve Order")
+            {
+                Console.WriteLine("Waiter is serving the order");
+            }
+            else if (nextEmployee != null)
+            {
+                nextEmployee.PerformTask(task);
+            }
+        }
     }
 
-    public class Manager
+    public class Manager : Employee
     {
-
+        public override void PerformTask(string task)
+        {
+            if (task == "Manage menu")
+            {
+                Console.WriteLine("Manager is managing menu");
+            }
+            else if (nextEmployee != null)
+            {
+                nextEmployee.PerformTask(task);
+            }
+        }
     }
 
-    public class Chef
+    public class Chef : Employee
     {
-
+        public override void PerformTask(string task)
+        {
+            if (task == "Prepare Dish")
+            {
+                Console.WriteLine("Chef is Preparing the dish");
+            }
+            else if (nextEmployee != null)
+            {
+                nextEmployee.PerformTask(task);
+            }
+        }
     }
 }
